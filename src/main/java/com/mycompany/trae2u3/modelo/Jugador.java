860d6cdf04ba2456573e7172d6531d65039d95a7
LAB-2 <LAB-2@DESKTOP-9OQBEH7>
2022-11-11 11:40:22 -0500

    Terminado

---
+++ src/main/java/com/mycompany/trae2u3/modelo/Jugador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trae2u3.modelo;
+
+/**
+ *
+ * @author LAB-2
+ */
+public class Jugador {
+    
+}
---
+++ src/main/java/com/mycompany/trae2u3/vista/Trae2u3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.trae2u3.vista;
+
+import com.mycompany.trae2u3.*;
+
+/**
+ *
+ * @author LAB-2
+ */
+public class Trae2u3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
